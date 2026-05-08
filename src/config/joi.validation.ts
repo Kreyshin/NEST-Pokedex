2c984220dab845f1b35bc40714b57779d5f2a183
Kreyshin <kreyshin@gmail.com>
2026-05-08 14:47:56 -0500

    dasdad
    asdasd

--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -1,10 +1,6 @@
 import * as Joi from "joi";
 export const JoiValidationSchema = Joi.object({
-<<<<<<< HEAD
   NODE_ENV: Joi.string().valid('dev', 'prod', 'test','production').default('dev'),
-=======
-  NODE_ENV: Joi.string().valid('dev', 'prod', 'test').default('dev'),
->>>>>>> origin/main
   MONGODB: Joi.required().default('mongodb://localhost:27017/nest'),      
   PORT: Joi.number().default(4000),
   DEFAULT_LIMIT: Joi.number().default(10),
